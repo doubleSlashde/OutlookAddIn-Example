57210e3a31c568c4d40afc8bfb99a630f33ac45a
jdeigendesch <jennifer.deigendesch@doubleslash.de>
2016-07-13 10:39:20 +0200

    Added ribbon button "smile"

---
+++ OutlookAddIn-Example/OutlookAddIn-Example/Ribbon.cs
@@ -0,0 +1,54 @@
+﻿using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+using Office = Microsoft.Office.Core;
+
+namespace OutlookAddIn_Example {
+
+   [ComVisible(true)]
+   public class Ribbon : Office.IRibbonExtensibility {
+      private const string GroupSmile = "Smileys";
+      private const string Smile = "Smile";
+      private const string TooltipSmile = "This is a tooltip to smile.";
+
+      private Office.IRibbonUI _ribbon;
+
+      #region IRibbonExtensibility Members
+
+      public string GetCustomUI(string ribbonId) {
+         return Resource.Ribbon;
+      }
+
+      #endregion
+
+      #region Ribbon Callbacks
+
+      //Create callback methods here. For more information about adding callback methods, visit http://go.microsoft.com/fwlink/?LinkID=271226
+
+      public void Ribbon_Load(Office.IRibbonUI ribbonUi) {
+         _ribbon = ribbonUi;
+      }
+
+      public string Group_GetLabel(Office.IRibbonControl control) {
+         return GroupSmile;
+      }
+
+      public Bitmap Button_GetImage(Office.IRibbonControl control) {
+         return Resource.Smile;
+      }
+
+      public void Button_Click(Office.IRibbonControl control) {
+         MessageBox.Show(Smile);
+      }
+
+      public string Button_GetTip(Office.IRibbonControl control) {
+         return TooltipSmile;
+      }
+
+      public string Button_GetLabel(Office.IRibbonControl control) {
+         return Smile;
+      }
+
+      #endregion
+   }
+}
--- OutlookAddIn-Example/OutlookAddIn-Example/ThisAddIn.cs
+++ OutlookAddIn-Example/OutlookAddIn-Example/ThisAddIn.cs
@@ -1,37 +1,31 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Xml.Linq;
-using Outlook = Microsoft.Office.Interop.Outlook;
 using Office = Microsoft.Office.Core;
 
-namespace OutlookAddIn_Example
-{
-    public partial class ThisAddIn
-    {
-        private void ThisAddIn_Startup(object sender, System.EventArgs e)
-        {
-        }
+namespace OutlookAddIn_Example {
+   public partial class ThisAddIn {
+      private void ThisAddIn_Startup(object sender, EventArgs e) {
+      }
 
-        private void ThisAddIn_Shutdown(object sender, System.EventArgs e)
-        {
-            // Note: Outlook no longer raises this event. If you have code that 
-            //    must run when Outlook shuts down, see http://go.microsoft.com/fwlink/?LinkId=506785
-        }
+      private void ThisAddIn_Shutdown(object sender, EventArgs e) {
+         // Note: Outlook no longer raises this event. If you have code that 
+         //    must run when Outlook shuts down, see http://go.microsoft.com/fwlink/?LinkId=506785
+      }
 
-        #region VSTO generated code
+      protected override Office.IRibbonExtensibility CreateRibbonExtensibilityObject() {
+         return new Ribbon();
+      }
 
-        /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
-        /// </summary>
-        private void InternalStartup()
-        {
-            this.Startup += new System.EventHandler(ThisAddIn_Startup);
-            this.Shutdown += new System.EventHandler(ThisAddIn_Shutdown);
-        }
-        
-        #endregion
-    }
+      #region VSTO generated code
+
+      /// <summary>
+      ///    Required method for Designer support - do not modify
+      ///    the contents of this method with the code editor.
+      /// </summary>
+      private void InternalStartup() {
+         Startup += ThisAddIn_Startup;
+         Shutdown += ThisAddIn_Shutdown;
+      }
+
+      #endregion
+   }
 }
